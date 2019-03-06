130b5bdfdaf6eaa83468e267bef659e1b772bd2d
GuiGui <bague.guillaume@live.fr>
2019-03-06 20:26:35 +0100

    Add checkUserAuth() method + Connexion page layout
    Check if user is connect

--- app/src/main/java/com/android/guillaume/go4launch/MainActivity.java
+++ app/src/main/java/com/android/guillaume/go4launch/MainActivity.java
@@ -2,12 +2,37 @@ package com.android.guillaume.go4launch;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.auth = FirebaseAuth.getInstance();
+
+        this.checkUserAuth();
+
         setContentView(R.layout.activity_main);
     }
+
+    private void checkUserAuth() {
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+        if (currentUser != null){
+            //User is sign in
+            Toast.makeText(this, "Already connect", Toast.LENGTH_LONG).show();
+        } else{
+            //User isn't sign in
+            Toast.makeText(this, "Not connect", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
